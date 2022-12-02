fd49eedf36744fd72bfdb904cdd24687ab3b1cc2
Timofey_Viligdanov <tim_vi_ov@mail.ru>
2022-12-02 14:02:00 +0300

    add folder Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве. 
+// Задание должно быть выполнено в методе. 
+// В методе не должно быть вывода в консоль.
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+Console.WriteLine("Введите координату Х точки А");
+double Ax = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки А");
+double Ay = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Z точки А");
+double Az = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Х точки B");
+double Bx = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки B");
+double By = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Z точки B");
+double Bz = double.Parse(Console.ReadLine());
+
+
+
+//Обращаемся к методу для получения результата его(метода) обработки.
+double Distance =  DistanceBetweenPoints(Ax, Ay, Az, Bx, By, Bz);
+
+Console.WriteLine("Расстояние между точками А и В в 3х мерном пространстве равно:");
+Console.WriteLine(Distance);
+
+
+//Создаем метод который принимает введенные значения, обрабатывает их и возвращает рез-т.
+double DistanceBetweenPoints(double Ax, double Ay, 
+                            double Az, double Bx, 
+                            double By, double Bz )
+{
+   
+    double sum = Math.Pow((Bx-Ax),2) + Math.Pow((By-Ay),2) + Math.Pow((Bz-Az),2);
+    
+    double distance = Math.Pow(sum, 0.5);
+   
+    return distance;
+
+    for( int i = 0; i < 2; i++)
+    {
+        Console.WriteLine("distance равно:");
+        Console.WriteLine(distance);
+    }
+}
